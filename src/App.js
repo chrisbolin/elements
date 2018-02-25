1bc19eda38ca897fa723a9c34396f8978da8d2e3
Chris Bolin <bolin.chris@gmail.com>
2018-02-25 11:14:29 -0700

    demo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Scroller from "./Scroller";
 
 const Page = ({ data, index, progress }) => {
   const style = {
-    transform: `scale(${Math.max(progress, 0.5)})`,
+    transform: `scale(${Math.max(progress, 0.8)})`,
     border: `10px solid hsla(${70 * index}, 100%, 45%, 0.3)`
   };
   return (
