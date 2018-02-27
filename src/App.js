3ba96ac2ee72fc8024cb38b6fbf5874843be90e3
Chris Bolin <bolin.chris@gmail.com>
2018-02-26 23:22:15 -0700

    page styling

--- src/App.js
+++ src/App.js
@@ -17,40 +17,56 @@ import Cover from "./Cover";
 
 const padToTwo = n => (n < 10 ? `0${n}` : n);
 
-const Page = ({ component, number, total }) => {
+const Page = ({
+  component,
+  number,
+  total,
+  footer = null,
+  footerClassName = ""
+}) => {
   return (
     <div className="page">
       {createElement(component)}
       <div className="number">
         {padToTwo(number)} of {padToTwo(total)}
       </div>
+      {footer && <div className={`footer ${footerClassName}`}>{footer}</div>}
     </div>
   );
 };
 
-const pages = [
-  Contact01,
+const firstPiece = Contact11;
+const lastPiece = Contact02;
+
+const otherPieces = [
+  Contact06,
   Contact13,
   Contact05,
-  Contact06,
   Contact07,
   Contact04,
+  Contact01,
   Contact09,
   Contact10,
   Contact12,
-  Contact15,
-  Contact02
+  Contact15
 ];
 
-const total = pages.length + 1;
+const total = otherPieces.length + 2;
 
 const App = () => (
   <div className="app">
-    <Page component={Contact11} number={1} total={total} />
+    <Page
+      component={firstPiece}
+      number={1}
+      total={total}
+      footer="↓"
+      footerClassName="first-footer"
+    />
     <Cover />
-    {pages.map((page, index) => (
+    {otherPieces.map((page, index) => (
       <Page component={page} key={index} number={index + 2} total={total} />
     ))}
+    <Page component={lastPiece} number={total} total={total} footer="(end)" />
   </div>
 );
 
