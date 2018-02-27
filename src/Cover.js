f779516441e52ce37b705b389ce6c27fe9f3cb14
Chris Bolin <bolin.chris@gmail.com>
2018-02-27 00:32:34 -0700

    correct for huge screens

--- src/Cover.js
+++ src/Cover.js
@@ -8,16 +8,19 @@ const Link = ({ children, ...props }) => (
 
 const Cover = () => (
   <div className="cover">
-    <div className="title">elements</div>
-    <div className="text body">
-      Elements is a digital art series which aims to capture the beauty in
-      simple interactions. ■ These pieces are neither images nor renderings.
-      They are just a handful of elements (using two simple building blocks of
-      the web, html and css). ■ Elements was created for the inaugural issue of{" "}
-      <Link href="https://thedisconnect.co/one/">The Disconnect</Link>.
-    </div>
-    <div className="text footnote">
-      <Link href="https://chris.bolin.co">Chris Bolin</Link>, 2018
+    <div className="contents">
+      <div className="title">elements</div>
+      <div className="text body">
+        Elements is a digital art series which aims to capture the beauty in
+        simple interactions. ■ These pieces are neither images nor renderings.
+        They are just a handful of elements (using two simple building blocks of
+        the web, html and css). ■ Elements was created for the inaugural issue
+        of <Link href="https://thedisconnect.co/one/">The Disconnect</Link>.
+      </div>
+      <div className="text footnote">
+        <Link href="https://chris.bolin.co">Chris Bolin</Link>, 2018
+      </div>
+      <div className="footer first-footer">↓</div>
     </div>
   </div>
 );
